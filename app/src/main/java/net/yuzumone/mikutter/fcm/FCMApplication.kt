8a3303f3c2a07d8d4cbc95ef95a83668da631feb
yuzumone <yuzumone@outlook.com>
2018-08-01 07:01:11 +0900

    Add fabric crashlytics
    close #8

--- app/src/main/java/net/yuzumone/mikutter/fcm/FCMApplication.kt
+++ app/src/main/java/net/yuzumone/mikutter/fcm/FCMApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.analytics.FirebaseAnalytics
+import io.fabric.sdk.android.Fabric
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 
 class FCMApplication : Application() {
 
@@ -14,5 +17,10 @@ class FCMApplication : Application() {
                 .setApiKey(BuildConfig.API_KEY)
                 .build(), getString(R.string.app_name))
         FirebaseAnalytics.getInstance(this)
+        val core = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG).build()
+        val crashlytics = Crashlytics.Builder()
+                .core(core).build()
+        Fabric.with(this, crashlytics)
     }
 }
