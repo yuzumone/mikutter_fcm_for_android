be09c72a1a2a5eb2b2bdec72975ffc9e630d7053
yuzumone <yuzumone@outlook.com>
2018-08-06 21:10:13 +0900

    Add mikutter message dao

---
+++ app/src/main/java/net/yuzumone/mikutter/fcm/db/MikutterMessageDao.kt
@@ -0,0 +1,24 @@
+package net.yuzumone.mikutter.fcm.db
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import net.yuzumone.mikutter.fcm.entity.MikutterMessage
+
+@Dao
+interface MikutterMessageDao {
+
+    @Query("SELECT * from message ")
+    fun findAll(): LiveData<List<MikutterMessage>>
+
+    @Query("SELECT * FROM message WHERE id = :id ")
+    fun findOne(id: String): LiveData<MikutterMessage>
+
+    @Insert
+    fun insert(message: MikutterMessage)
+
+    @Delete
+    fun delete(message: MikutterMessage)
+}
