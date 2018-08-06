15852b88a7e8f4be40f41bd7877396592d07d3a1
yuzumone <yuzumone@outlook.com>
2018-08-06 21:12:17 +0900

    Init database in application

--- app/src/main/java/net/yuzumone/mikutter/fcm/FCMApplication.kt
+++ app/src/main/java/net/yuzumone/mikutter/fcm/FCMApplication.kt
@@ -1,14 +1,20 @@
 package net.yuzumone.mikutter.fcm
 
 import android.app.Application
+import android.arch.persistence.room.Room
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.analytics.FirebaseAnalytics
 import io.fabric.sdk.android.Fabric
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
+import net.yuzumone.mikutter.fcm.db.MikutterMessageDatabase
 
 class FCMApplication : Application() {
+
+    companion object {
+        lateinit var database: MikutterMessageDatabase
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -22,5 +28,7 @@ class FCMApplication : Application() {
         val crashlytics = Crashlytics.Builder()
                 .core(core).build()
         Fabric.with(this, crashlytics)
+        database = Room.databaseBuilder(this,
+                MikutterMessageDatabase::class.java, "database").build()
     }
 }
