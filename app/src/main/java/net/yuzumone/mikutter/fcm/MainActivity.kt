7b8120ae2a043e4f8bc04b484bf861491a744536
yuzumone <yuzumone@outlook.com>
2018-08-10 08:24:25 +0900

    Add message list fragment
    close #14

--- app/src/main/java/net/yuzumone/mikutter/fcm/MainActivity.kt
+++ app/src/main/java/net/yuzumone/mikutter/fcm/MainActivity.kt
@@ -2,16 +2,35 @@ package net.yuzumone.mikutter.fcm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import net.yuzumone.mikutter.fcm.ui.setting.SettingFragment
+import android.view.MenuItem
+import net.yuzumone.mikutter.fcm.ui.list.MessageListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
-            val settingFragment = SettingFragment()
+            val fragment = MessageListFragment()
             supportFragmentManager.beginTransaction()
-                    .add(android.R.id.content, settingFragment).commit()
+                    .add(android.R.id.content, fragment).commit()
+            supportFragmentManager.addOnBackStackChangedListener {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                } else {
+                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                }
+            }
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
